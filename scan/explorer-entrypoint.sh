affa9ec60d2dcf7c71eb4da08d6ab416bf0b3a45
Kim <dreamcacao02183@gmail.com>
2025-09-26 11:53:01 +0900

    scan: replace op scan to blockscout

--- scan/explorer-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-
-cd /app/op-scan && cp /data/.env.local .env.local
-export $(grep -v '^#' .env.local | xargs)
-
-pnpm build && pnpm start
--- scan/indexer-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-
-cd /app/op-scan && cp /data/.env.local .env.local
-export $(grep -v '^#' .env.local | xargs)
-
-pnpm prisma:db:push && pnpm op-indexer
